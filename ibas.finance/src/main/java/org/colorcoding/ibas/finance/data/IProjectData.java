99f320d927984178075ba570beb1d17ec87d0825
牛加人等于朱 <niuren.zhu@icloud.com>
2018-10-17 11:17:31 +0800

    增加项目，过账期间，维度功能

---
+++ ibas.finance/src/main/java/org/colorcoding/ibas/finance/data/IProjectData.java
@@ -0,0 +1,17 @@
+package org.colorcoding.ibas.finance.data;
+
+/**
+ * 项目数据
+ * 
+ * @author Niuren.Zhu
+ *
+ */
+public interface IProjectData {
+
+	/**
+	 * 获取-项目代码
+	 * 
+	 * @return 值
+	 */
+	String getProject();
+}
